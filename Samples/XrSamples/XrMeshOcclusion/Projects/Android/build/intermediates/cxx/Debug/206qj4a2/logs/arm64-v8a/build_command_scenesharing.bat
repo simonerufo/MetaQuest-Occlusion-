8af90d4338bba8f9288bab5b76d27764162e21cb
simoneru <simonerufo3@gmail.com>
2025-08-04 11:24:04 +0200

    minor fixes

---
+++ Samples/XrSamples/XrMeshOcclusion/Projects/Android/build/intermediates/cxx/Debug/206qj4a2/logs/arm64-v8a/build_command_scenesharing.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\simoneru\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\simoneru\\Desktop\\Thesis\\Samples\\XrSamples\\XrMeshOcclusion\\Projects\\Android\\.cxx\\Debug\\206qj4a2\\arm64-v8a" ^
+  scenesharing
